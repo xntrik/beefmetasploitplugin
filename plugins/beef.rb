02a8ef20a20a47b01a50bdc2172ddc44edf93ae0
Christian Frichot <xntrik+git@gmail.com>
2011-08-03 21:34:51 +0800

    working on the beef_target commands - can now execute modules - but no params yet

--- plugins/beef.rb
+++ plugins/beef.rb
@@ -23,14 +23,21 @@ module Msf
           "beef_online" => "List available hooked browsers",
           "beef_offline" => "List previously hooked browsers",
           "beef_review" => "Review a previously hooked browser",
+          "beef_target" => "Target a currently hooked browser",
         }
       end
       
       @@beef_review_opts = Rex::Parser::Arguments.new(
         "-h" => [ false, "Help."],
         "-i" => [ true, "Display info about the offline hooked browser. \"beef_review -i <id>\""],
-        "-l" => [ true, "List previously executed modules. \"beef_review -l <id>\""],
-        "-r" => [ true, "Review the response from a previously executed command module. \"beef_review -r <id> <command id>\""])
+        "-r" => [ true, "Review the response from a previously executed command module. \"beef_review -r <id> (<command id>)\""])
+        
+      @@beef_target_opts = Rex::Parser::Arguments.new(
+        "-h" => [ false, "Help."],
+        "-i" => [ true, "Display info about the online hooked browser (target). \"beef_target -i <id>\""],
+        "-r" => [ true, "Review the response from a previously executed command module. \"beef_target -r <id> (<command id>)\""],
+        "-c" => [ true, "List available commands for a particular target. \"beef_target -c <id> (<command id>)\""],
+        "-e" => [ true, "Execute a module against a target. \"beef_target -e <id> <command id>\""])
       
       #Thank you Nessus plugin for this!
       def beef_verify_db
@@ -132,6 +139,7 @@ module Msf
         tbl << [ "beef_offline","List previously hooked browsers and their details."]
         tbl << [ "beef_import", "Import available hooked browsers into db_hosts."]
         tbl << [ "beef_review", "Review previously hooked browsers within BeEF."]
+        tbl << [ "beef_target", "Target a currently hooked browser within BeEF."]
         puts "\n"
         puts tbl.to_s + "\n"
       end
@@ -177,8 +185,6 @@ module Msf
       def cmd_beef_import_help
         print_status("Import available hooked browsers into db_hosts.")
       end
-      
-      
       
       def cmd_beef_online(*args)
         hb = nil
@@ -240,6 +246,153 @@ module Msf
         puts tbl.to_s + "\n"
       end
       
+      def cmd_beef_target(*args)
+        if (args[0] == nil or args[0] == "-h")
+          print_status("Listing online browsers...")
+          cmd_beef_online
+          cmd_beef_target_help
+          return
+        end
+        
+        if @remotebeef.session.connected.nil?
+          print_status("You aren't connected")
+          return
+        end
+        
+        @@beef_target_opts.parse(args) {|opt, idx, val|
+          case opt
+          when "-i"
+            if args.length < 2
+              cmd_beef_target_help
+              return
+            end
+            @remotebeef.settarget(val)
+            info = @remotebeef.zombiepoll.getinfo(@remotebeef.targetsession)
+            info['results'].each { |x|
+              x['data'].each { |k,v|
+                print_line(k+ " - "+v)
+              }
+            }
+            return
+          when "-r"
+            if args.length < 2
+              cmd_beef_target_help
+              return
+            end
+            
+            if args[2].nil?
+              @remotebeef.settarget(val)
+              cmds = @remotebeef.command.getcommands(@remotebeef.targetsession)
+              tbl = Rex::Ui::Text::Table.new(
+                'Columns' =>
+                  ['Command Id',
+                    'Command',
+                    'Execute Count'
+                  ])
+              cmds.each{ |x|
+                x['children'].each { |y|
+                  tbl << [y['id'].to_s,
+                          x['text'].sub(/\W\(\d.*/,"")+"/"+y['text'].gsub(/[-\(\)]/,"").gsub(/\W+/,"_"),
+                          @remotebeef.command.getcmdexeccount(@remotebeef.targetsession,y['id'])] if @remotebeef.command.getcmdexeccount(@remotebeef.targetsession,y['id']) > 0
+                }
+              }
+              puts "\n"
+              puts "List of previous command modules for this target\n"
+              puts tbl.to_s + "\n"
+              return
+            else
+              @remotebeef.settarget(args[1])
+              @remotebeef.command.setmodule(args[2])
+                tbl = Rex::Ui::Text::Table.new(
+                  'Columns' =>
+                    [
+                      'Response Id',
+                      'Executed Time',
+                      'Response'
+                    ])
+              @remotebeef.command.getcmdresponses(@remotebeef.targetsession)['commands'].each do |resp|
+                indiresp = @remotebeef.command.getindividualresponse(resp['object_id'])
+                respout = ""
+                if indiresp['results'].length == 0 or indiresp == nil
+                  respout = "No response yet"
+                  respdata = ""
+                else
+                  respout = Time.at(indiresp['results'][0]['date'].to_i).to_s
+                  respdata = indiresp['results'][0]['data']['data'].to_s
+                end
+                tbl << [resp['object_id'].to_s,resp['creationdate'],respout]
+                tbl << [respdata,"",""]
+              end
+              puts "\n"
+              puts "List of responses for this command module\n"
+              puts tbl.to_s + "\n"
+              return
+            end
+          when "-c"
+            if args[2].nil?
+              @remotebeef.settarget(args[1])
+              cmds = @remotebeef.command.getcommands(@remotebeef.targetsession)
+              tbl = Rex::Ui::Text::Table.new(
+                'Columns' =>
+                  [
+                    'Id',
+                    'Command',
+                    'Execute Count'
+                  ])
+              cmds.each{ |x|
+                x['children'].each{ |y|
+                  tbl << [y['id'].to_s, x['text'].sub(/\W\(\d.*/,"")+"/"+y['text'].gsub(/[-\(\)]/,"").gsub(/\W+/,"_"),@remotebeef.command.getcmdexeccount(@remotebeef.targetsession,y['id'])]
+                }
+              }
+              puts "\n"
+              puts "List of command modules for this target\n"
+              puts tbl.to_s + "\n"
+            else
+              @remotebeef.settarget(args[1])
+              @remotebeef.command.setmodule(args[2])
+              print_line("Module name: " + @remotebeef.command.cmd['Name'])
+              print_line("Module category: " + @remotebeef.command.cmd['Category'])
+              print_line("Module description: " + @remotebeef.command.cmd['Description'])
+              print_line("Module parameters:")
+
+              @remotebeef.command.cmd['Data'].each{|data|
+                print_line(data['name'] + " => " + data['value'] + " # this is the " + data['ui_label'] + " parameter")
+              } if not @remotebeef.command.cmd['Data'].nil?
+            end
+            return
+          when "-e"
+            if args.length < 3
+              cmd_beef_target_help
+              return
+            else
+              @remotebeef.settarget(args[1])
+              @remotebeef.command.setmodule(args[2])
+              if not args[3].nil?
+                #Therefore we have some parameters too #xntrik TODO - fix this ?
+                pstring = ""
+                (3..args.length-1).each do |x|
+                  pstring << args[x] << " "
+                end
+                pstring.chop!
+              end
+              @remotebeef.command.runmodule(@remotebeef.targetsession).nil? ? print_status("Command not sent") : print_status("Command sent")
+              return
+            end
+          when "-h"
+            cmd_beef_target_help
+            return
+          else
+            cmd_beef_target_help
+            return
+          end
+        }
+      end
+      
+      def cmd_beef_target_help
+        print_status("Use the \"target\" commands to interface with online, hooked browsers")
+        print @@beef_target_opts.usage()
+      end
+      
       def cmd_beef_review(*args)
         if (args[0] == nil or args[0] == "-h")
           print_status("Listing offline browsers...")
@@ -256,6 +409,10 @@ module Msf
         @@beef_review_opts.parse(args) {|opt, idx, val|
           case opt
             when "-i"
+              if args.length < 2
+                cmd_beef_review_help
+                return
+              end
               @remotebeef.setofflinetarget(val)
               info = @remotebeef.zombiepoll.getinfo(@remotebeef.targetsession)
               info['results'].each { |x|
@@ -263,48 +420,55 @@ module Msf
                   print_line(k+ " - "+v)
                 }
               }
-            when "-l"
-              @remotebeef.setofflinetarget(val)
-              cmds = @remotebeef.command.getcommands(@remotebeef.targetsession)
-              tbl = Rex::Ui::Text::Table.new(
-                'Columns' =>
-                  [
-                    'Command Id',
-                    'Command',
-                    'Execute Count'
-                  ])
-              cmds.each{ |x|
-                x['children'].each{ |y|
-                  tbl << [y['id'].to_s,
-                          x['text'].sub(/\W\(\d.*/,"")+"/"+y['text'].gsub(/[-\(\)]/,"").gsub(/\W+/,"_"),
-                          @remotebeef.command.getcmdexeccount(@remotebeef.targetsession,y['id'])] if @remotebeef.command.getcmdexeccount(@remotebeef.targetsession,y['id']) > 0
-                }
-              }
-              puts "\n"
-              puts "List of previous command modules for this target\n"
-              puts tbl.to_s + "\n"
             when "-r"
-              @remotebeef.setofflinetarget(args[1])
-              @remotebeef.command.setmodule(args[2])
-              tbl = Rex::Ui::Text::Table.new(
-                'Columns' =>
-                  [
-                    'Response Id',
-                    'Executed Time',
-                    'Response'
-                  ])
-              @remotebeef.command.getcmdresponses(@remotebeef.targetsession)['commands'].each do |resp|
-                indiresp = @remotebeef.command.getindividualresponse(resp['object_id'])
-                respout = ""
-                if indiresp['results'].length == 0 or indiresp == nil
-                  respout = "No response yet"
-                  respdata = ""
-                else
-                  respout = Time.at(indiresp['results'][0]['date'].to_i).to_s
-                  respdata = indiresp['results'][0]['data']['data'].to_s
+              if args.length < 2
+                cmd_beef_review_help
+                return
+              end
+              if args[2].nil?
+                @remotebeef.setofflinetarget(args[1])
+                cmds = @remotebeef.command.getcommands(@remotebeef.targetsession)
+                tbl = Rex::Ui::Text::Table.new(
+                  'Columns' =>
+                    [
+                      'Command Id',
+                      'Command',
+                      'Execute Count'
+                    ])
+                cmds.each{ |x|
+                  x['children'].each{ |y|
+                    tbl << [y['id'].to_s,
+                            x['text'].sub(/\W\(\d.*/,"")+"/"+y['text'].gsub(/[-\(\)]/,"").gsub(/\W+/,"_"),
+                            @remotebeef.command.getcmdexeccount(@remotebeef.targetsession,y['id'])] if @remotebeef.command.getcmdexeccount(@remotebeef.targetsession,y['id']) > 0
+                  }
+                }
+                puts "\n"
+                puts "List of previous command modules for this target\n"
+                puts tbl.to_s + "\n"
+                return
+              else
+                @remotebeef.setofflinetarget(args[1])
+                @remotebeef.command.setmodule(args[2])
+                tbl = Rex::Ui::Text::Table.new(
+                  'Columns' =>
+                    [
+                      'Response Id',
+                      'Executed Time',
+                      'Response'
+                    ])
+                @remotebeef.command.getcmdresponses(@remotebeef.targetsession)['commands'].each do |resp|
+                  indiresp = @remotebeef.command.getindividualresponse(resp['object_id'])
+                  respout = ""
+                  if indiresp['results'].length == 0 or indiresp == nil
+                    respout = "No response yet"
+                    respdata = ""
+                  else
+                    respout = Time.at(indiresp['results'][0]['date'].to_i).to_s
+                    respdata = indiresp['results'][0]['data']['data'].to_s
+                  end
+                  tbl << [resp['object_id'].to_s,resp['creationdate'],respout]
+                  tbl << [respdata,"",""]
                 end
-                tbl << [resp['object_id'].to_s,resp['creationdate'],respout]
-                tbl << [respdata,"",""]
               end
               puts "\n"
               puts "List of responses for this command module\n"
